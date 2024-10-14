56b4b24339f4a2112de27cee4bdbee934621b6b7
MitendraSingh <mitendrasingh.chauhan@gmail.com>
2024-10-14 17:12:15 +0530

    committing first program

---
+++ src/IntroductionTOGit.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mchauhan31
+ * Date: 14/10/24
+ * Time: 5:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IntroductionTOGit {
+    public static void main (String args[]){
+        System.out.println("Hello git training program");
+    }
+}
