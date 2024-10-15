9e31ce8ebe0056c5facb35ea4d7f0354e9c14c46
MitendraSingh <mitendrasingh.chauhan@gmail.com>
2024-10-15 14:42:42 +0530

    updated

--- src/IntroductionTOGit.java
+++ src/IntroductionTOGit.java
@@ -8,5 +8,6 @@
 public class IntroductionTOGit {
     public static void main (String args[]){
         System.out.println("Hello git training program");
+        new PrintNumbers().printTen();
     }
 }
